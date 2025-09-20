8d5bacca8eab35712ef724ba50f12e435daf3fb5
Yaroslav <36301121+Ceotom@users.noreply.github.com>
2025-09-20 16:08:32 +0500

    Update merge.ps1

--- scripts/merge.ps1
+++ scripts/merge.ps1
@@ -3,7 +3,8 @@ param (
 )
 
 if ($Configuration -eq "Release") {
-    & ilrepack /out:out\ablauncher.exe /verbose ablauncher.exe Newtonsoft.Json.dll ru\ablauncher.resources.dll
+    & ilrepack /out:out\ablauncher.exe /verbose ablauncher.exe Newtonsoft.Json.dll
     
     Copy-Item -Force LICENSE out\LICENSE
+    Copy-Item -Force ru\ablauncher.resources.dll out\ru\ablauncher.resources.dll
 }
